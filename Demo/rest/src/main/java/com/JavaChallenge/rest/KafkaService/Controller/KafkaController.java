51bf70c0b6d53d5204d0d1818ba1f8c0705245c0
Diogo Fernandes <170881084+Diogobfer@users.noreply.github.com>
2025-09-26 14:41:36 +0100

    Add Kafka service

---
+++ Demo/rest/src/main/java/com/JavaChallenge/rest/KafkaService/Controller/KafkaController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.JavaChallenge.rest.KafkaService.Controller;
+
+import com.JavaChallenge.rest.KafkaService.Service.KafkaProducerService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ *
+ * @author Administrator
+ */
+@RestController
+public class KafkaController {
+    private final KafkaProducerService kafkaProducerService;
+
+    public KafkaController(KafkaProducerService kafkaProducerService) {
+        this.kafkaProducerService = kafkaProducerService;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage(@RequestParam String message) {
+        kafkaProducerService.sendMessage(message);
+        return "Message sent successfully";
+    }
+}
---
+++ Demo/rest/src/main/java/com/JavaChallenge/rest/KafkaService/Service/KafkaProducerService.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.JavaChallenge.rest.KafkaService.Service;
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+/**
+ *
+ * @author Administrator
+ */
+@Service
+public class KafkaProducerService {
+     private static final String TOPIC = "my_topic";
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String message) {
+        kafkaTemplate.send(TOPIC, message);
+        System.out.println("Message sent: " + message);
+    }
+}
