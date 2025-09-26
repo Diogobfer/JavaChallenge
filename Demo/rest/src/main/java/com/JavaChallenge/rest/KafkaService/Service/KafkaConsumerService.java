8bd51562430e94425f896234887de1c37b7107c0
Diogo Fernandes <170881084+Diogobfer@users.noreply.github.com>
2025-09-26 23:38:16 +0100

    Add kafka producer and consumer

--- Demo/rest/src/main/java/com/JavaChallenge/rest/KafkaService/Service/KafkaConsumerService.java
+++ Demo/rest/src/main/java/com/JavaChallenge/rest/KafkaService/Service/KafkaConsumerService.java
@@ -10,9 +10,56 @@ import org.springframework.stereotype.Service;
  * @author Administrator
  */
 @Service
-public class KafkaConsumerService {
-    @KafkaListener(topics = "topic", groupId = "com.JavaChallenge")
-    public void consume(String message) {
-        System.out.println("Message received: " + message);
+public class KafkaConsumerService implements InterfaceKafkaConsumerService{
+    
+    private String sumResult;
+    private String subtractionResult;
+    private String multiplicationResult;
+    private String divisionResult;
+
+    
+    @KafkaListener(topics = "sumResult", groupId = "com.JavaChallenge")
+    @Override
+    public void sumResult(String message) {
+        
+        this.sumResult = message;
+    }
+
+    @KafkaListener(topics = "subtractionResult", groupId = "com.JavaChallenge")
+    @Override
+    public void subtractionResult(String message) {
+        
+       this.subtractionResult= message;
+    }
+
+    @KafkaListener(topics = "multiplicationResult", groupId = "com.JavaChallenge")
+    @Override
+    public void multiplicationResult(String message) {
+       this.multiplicationResult = message;
+    }
+
+    @KafkaListener(topics = "divisionResult", groupId = "com.JavaChallenge")
+    @Override
+    public void divisionResult(String message) {
+      this.divisionResult = message;
+    }
+   
+    public String getSumResult() {
+        return sumResult;
+    }
+
+   
+    public String getSubtractionResult() {
+        return subtractionResult;
+    }
+
+    
+    public String getMultiplicationResult() {
+        return multiplicationResult;
+    }
+
+   
+    public String getDivisionResult() {
+        return divisionResult;
     }
 }
