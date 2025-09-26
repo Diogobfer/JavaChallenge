e42d93c123f3ad7d88a30db74a665ecf4638ba10
Diogo Fernandes <170881084+Diogobfer@users.noreply.github.com>
2025-09-26 18:41:16 +0100

    fix Dockerfile and Kafka implementation

--- Demo/calculator/src/main/java/com/JavaChallenge/calculator/KafkaService/Service/KafkaProducerService.java
+++ Demo/calculator/src/main/java/com/JavaChallenge/calculator/KafkaService/Service/KafkaProducerService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class KafkaProducerService {
-     private static final String TOPIC = "my_topic_calculator";
+     private static final String TOPIC = "topic";
 
     private final KafkaTemplate<String, String> kafkaTemplate;
 
