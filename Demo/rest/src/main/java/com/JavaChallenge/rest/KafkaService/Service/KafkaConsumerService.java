86e692f655bf74f45cb5ebce70d6c221acdbc7df
Diogo Fernandes <170881084+Diogobfer@users.noreply.github.com>
2025-09-26 14:45:01 +0100

    Kafka topic teste config

--- Demo/rest/src/main/java/com/JavaChallenge/rest/KafkaService/Service/KafkaConsumerService.java
+++ Demo/rest/src/main/java/com/JavaChallenge/rest/KafkaService/Service/KafkaConsumerService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class KafkaConsumerService {
-    @KafkaListener(topics = "my_topic", groupId = "group_id")
+    @KafkaListener(topics = "my_topic_rest", groupId = "com.JavaChallenge")
     public void consume(String message) {
         System.out.println("Message received: " + message);
     }
